d3450da4f9b6e875bdeb259211e1bd5fd76dd09f
vmolchanova <139213113+mvarukha@users.noreply.github.com>
2025-11-18 00:18:46 +0700

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -3,8 +3,8 @@ def count(n: int) -> int:
     Recursively calculates the number of digits in a natural number n.
     """
     if n < 10:
-        return 1
-    return 1 + count(n // 10)
+        return 1  # Base case: if n < 10, then it has 1 digit.
+    return 1 + count(n // 10)  # We add 1 and move on to the next digit.
 
 
 def main() -> None:
@@ -14,16 +14,16 @@ def main() -> None:
     try:
         n = int(input())
         if n < 0:
-            print("Error: n must be a natural number.")
+            print("n must be a natural number.")
             return
         # In case "n = 0".
         if n == 0:
-            print(1)
+            print(1)  # The number 0 is one digit.
             return
         result = count(n)
         print(result)
     except ValueError:
-        print("Error: invalid input.")
+        print("Invalid input.")  # Обрабатываем ошибку ввода.
 
 
 if __name__ == "__main__":
